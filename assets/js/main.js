b469af56f1c6eed5708e127a65663694ef6fe2ef
JordannaDelahoz <jddelahoz@uc.cl>
2024-08-05 23:31:12 -0400

    creando archivo base de main.js

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+
+
+
+/*----------Clase que contiene a los productos----------*/
+
+class Productos {
+    nombre = '';
+    precio = 0;
+
+    mostrarDetalles() {
+      console.log(`Nombre: ${this.nombre}, Precio: ${this.precio}`);
+    }
+}
+
+
+// En algún lado debo preguntarle al usuario donde debo ingresar los productos
+
+/*----------Clase con array vacío para almacenar productos----------*/
+class Carrito {
+    constructor(){
+        this.productos = [];
+    }
+};
+
+
+
+
+/*----------Función que permite agregar productos al carrito----------*/
+function agregarProductoCarro () {
+    if (Productos.venta == true) {
+
+    }
+}
+
+/*----------Función que permite calcular el total de toda la compra----------*/
+function calcularTotalCompra (){
+
+}
+
+
+/*----------Función que permite mostrar los detalles de la compra----------*/
+
+function mostrarDetallesCompra () {
+
+}
